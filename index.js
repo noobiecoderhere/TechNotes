63760e95060591503db02174463e28a76d06dfd8
noobiecoderhere <rkrishna1997@gmail.com>
2023-01-02 21:00:06 +0530

    Basic 404 page

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
+
+// Set public as the default lookup for static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Render 404 page response as per request
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Page Not Found" });
+  } else {
+    res.type("txt").send("404 Page Not Found");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Express server running on port: ${PORT}`);
